ffe095a3e3fc4855db949fde267f033de04d37d5
halim13 <hasanudinhalim@gmail.com>
2023-03-11 18:46:49 +0700

    config

--- vite.config.js
+++ vite.config.js
@@ -36,8 +36,19 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import inject from "@rollup/plugin-inject";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),  
+    inject({
+      $: 'jquery',
+      "window.jQuery": "jquery",
+      jQuery: 'jquery',
+    }),
+  ],
+  optimizeDeps: {
+    include: ["jquery"],
+  },
 })
