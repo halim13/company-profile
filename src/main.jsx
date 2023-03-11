065f1642e362da5476f46abf9c94c72957696f39
halim13 <hasanudinhalim@gmail.com>
2023-03-11 17:00:32 +0700

    jquery

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import jQuery from "jquery";
+import { jQuery, $ } from "jquery";
 
-Object.assign(window, { $: jQuery, jQuery });
+Object.assign(window, { $, jQuery });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
