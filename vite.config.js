1c24eea119a0f0f381d3e0cc154215c6a5a3c29f
halim13 <hasanudinhalim@gmail.com>
2023-03-11 18:36:05 +0700

    config

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,43 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import inject from "@rollup/plugin-inject";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     inject({
+//       $: 'jquery',
+//       "window.jQuery": "jquery",
+//       jQuery: 'jquery',
+//     }),
+//     react()],
+//   optimizeDeps: {
+//     include: ["jquery"],
+//   },
+//   root: './',
+//   build: {
+//     outDir: 'dist',
+//     rollupOptions: {
+//       output: {
+//         assetFileNames: (assetInfo) => {
+//           let extType = assetInfo.name.split('.').pop();
+//           if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+//             extType = 'img';
+//           }
+//           return `assets/${extType}/[name]-[hash][extname]`;
+//         },
+//         chunkFileNames: 'assets/js/[name]-[hash].js',
+//         entryFileNames: 'assets/js/[name]-[hash].js',
+//       },
+//     },
+//   },
+//   publicDir: 'assets'
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import inject from "@rollup/plugin-inject";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    inject({
-      $: 'jquery',
-      "window.jQuery": "jquery",
-      jQuery: 'jquery',
-    }),
-    react()],
-  optimizeDeps: {
-    include: ["jquery"],
-  },
-  root: './',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.').pop();
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'img';
-          }
-          return `assets/${extType}/[name]-[hash][extname]`;
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-      },
-    },
-  },
-  publicDir: 'assets'
+  plugins: [react()],
 })
